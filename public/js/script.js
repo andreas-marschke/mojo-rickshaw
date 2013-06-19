74102af6ea03c97852952922af53d1632beb32b2
Andreas Marschke <andreas.marschke@gmail.com>
2013-06-19 22:12:38 +0200

    Websocket based updates for Rickshaw Graphs

--- public/js/script.js
+++ public/js/script.js
@@ -6,33 +6,65 @@
         url: '/json',
         initialize: function() {
 	    _.extend(this,Backbone.Events);
+	    this.url = {};
+	    this.sockets = {};
+	    this.urls = {};
             this.palette = new Rickshaw.Color.Palette({scheme: "classic9"});
-            this.series = new Rickshaw.Series({
-                color: this.palette.color(),
-                data: []
-            });
+            this.series = new Rickshaw.Series.FixedDuration([{name:"_"}],this.pallete,{
+		color: this.palette.color(),
+		timeInterval: 100,
+		maxDataPoints: 100
+	    });
+	    var that = this;
+	    // get hosts available
+
+	    this.on("hosts_inited",function() {
+		this.transition();
+	    });
+	    this.on("urls_found",function(){
+		_.each(_.keys(that.urls),function(url){
+		    that.sockets[url] = new WebSocket(that.urls[url]);
+		    that.trigger('connected');
+		});
+	    });
+	    this.fetchHosts();
+	    this.on("change",function() {
+		if(this.length == 1) 
+		    that.trigger("first-data");
+	    });
         },
-        fetch: function() {
-            var that = this;
-            $.ajax({
-                type: 'GET',
-                url: this.url,
-                success: function(data) {
-                    that.add(new State(data));
-		    that.series.addData(data);
-		    if(that.length === 1) {
-			that.trigger('first-data');
-		    }
-		    that.trigger('series-update');
-                }
-            });
-        }
+	fetchHosts: function() {
+	    var that = this;
+	    $.ajax({
+		type: "GET",
+		url: '/hosts',
+		success: function(data) {
+		    that.hosts = data;
+		    that.trigger("hosts_inited");
+		}
+	    });
+	},
+	transition: function() {
+	    // Transition from AJAX to Websockets
+	    var that = this;
+	    $.ajax({
+		type : 'GET',
+		url: '/websockets',
+		success: function(data) {
+		    _.each(_.keys(data),function(url_name){ 
+			that.urls[url_name] = data[url_name];
+		    });
+		    that.trigger("urls_found");
+		}
+	    });
+	}
     });
 
     var StateView = Backbone.View.extend({
         collection: States,
         initialize: function() {
             this.graph = new Rickshaw.Graph({
+		renderer : 'line',
                 element: this.el,
                 width: 580,
                 height: 230,
@@ -50,7 +82,7 @@
 	    
 	    this.hoverDetail = new Rickshaw.Graph.HoverDetail({
 		graph: this.graph,
-		xFormatter: function(x) { return x + "seconds" },
+		xFormatter: function(x) { return x + "seconds"; },
 		yFormatter: function(y) { return Math.floor(y) + " percent" }
 	    });
 	    
@@ -75,14 +107,33 @@
     });
 
     var states = new States();
+    states.on('connected',function(){
+	_.each(_.keys(states.sockets),function(socket){
+	    states.sockets[socket].onopen = function() {};
+	    states.sockets[socket].onmessage = function(e) {
+		var data = JSON.parse(e.data);
+		states.add(new State(data));
+		states.series.addData(data);
+		if (states.length == 1 ) {
+		    /* Hotfix for buggy rendering of fixedDuration series*/
+		    if(states.series[0].name == "_") {
+			states.series.shift();
+		    }
+		    states.trigger('first-data');
+		    console.log(states.series[0]);
 
-    setInterval(function() {
-        states.fetch();
-    },1000);
+		} else {
+		    states.trigger('series-update');
+		}
+	    };
+	    states.sockets[socket].onerror = function(e) {
+		console.log(e);
+	    };
+	});
+	
+    });
     var view;
     states.on('first-data', function() { 
 	view = new StateView({el: $('#root'), collection: states});
     });
-
-
 })(this, this.document);
